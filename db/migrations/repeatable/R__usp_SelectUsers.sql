07ab40371a7283c7abc1cd356abc19cbaa0cd681
Maurício Fedatto <mfedatto@gmail.com>
2024-10-16 15:35:40 -0300

    Migrations: create table User, SP insert, SP select;

---
+++ db/migrations/repeatable/R__usp_SelectUsers.sql
@@ -0,0 +1,21 @@
+CREATE OR ALTER PROCEDURE usp_SelectUsers
+    @Username VARCHAR(32) = NULL,
+    @CreatedSince DATETIME = NULL,
+    @CreatedUntil DATETIME = NULL
+AS
+BEGIN
+
+    SELECT
+        UserId,
+        Username,
+        PasswordSalt,
+        PasswordHash,
+        CreatedAt
+    FROM
+        tbl_User
+    WHERE
+        (@Username IS NULL OR Username LIKE '%' + @Username + '%') AND
+        (@CreatedSince IS NULL OR CreatedAt >= @CreatedSince) AND
+        (@CreatedUntil IS NULL OR CreatedAt <= @CreatedUntil);
+
+END
---
+++ db/migrations/versioned/V20241016.0.1__Create_table_User.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tbl_User (
+    UserId INT PRIMARY KEY IDENTITY(1,1),
+    Username VARCHAR(32) NOT NULL,
+    PasswordSalt VARCHAR(16) NOT NULL,
+    PasswordHash VARCHAR(64) NOT NULL,
+    CreatedAt DATETIME DEFAULT GETDATE()
+);
