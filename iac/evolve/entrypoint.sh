1b1427dd588f12e77763184a1e69bbd39fcf95d3
Maurício Fedatto <mfedatto@gmail.com>
2024-10-20 21:50:22 -0300

    adicionados migration para criacao do banco e usuario do hangfire; iniciado Dockerfile para concentrar o setup e o migration num unico container;

---
+++ iac/evolve/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo Docker container made by MFedatto
+echo Thanks to the EvolveDB team for providing such a great tool
+echo
+echo COMMAND: ${COMMAND}
+echo DBMS: ${DBMS}
+echo CONNECTION_STRING: ${CONNECTION_STRING}
+echo LOCATIONS_ARGS:${LOCATIONS_ARGS}
+echo SCHEMA_ARGS:${SCHEMA_ARGS}
+echo PLACEHOLDERS_ARGS:${PLACEHOLDERS_ARGS}
+echo METADATA_TABLE_ARG: ${METADATA_TABLE_ARG}
+echo
